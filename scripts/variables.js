c676717492d91bf167d2ff26756c7292ba0b7426
courtney-swift <courtneydezman@gmail.com>
2026-05-17 03:59:13 +0900

    Variables

---
+++ scripts/variables.js
@@ -0,0 +1,60 @@
+console.log("===========")
+console.log("variables script");
+console.log("===========");
+
+//String (text) ""
+let firstName = "Courtney"
+//numerical 34, 67, 3.0
+let age = 99
+//boolean (true/false)
+let isStudent = true;
+let isProfessor = false;
+
+console.log(firstName);
+console.log(age);
+console.log(isStudent);
+
+let lastName = "Swift"
+let height = 180
+
+console.log(lastName);
+console.log(height + "cm");
+
+// gpa - number
+// address - String
+// allergies - boolean
+// zip - String
+// phone - String
+// If no math operation its a String
+
+// Arithmetic operations
+let num1 = 10;
+let num2 = 30;
+
+let sum = num1 + num2;
+let sub = num1 - num2;
+let mult = num1 * num2;
+let div = num1 / num2;
+
+//concatenation (join strings)
+console.log(num1 + " + " + num2 + " = " + sum);
+
+//string interpolation
+console.log(`${num1} + ${num2} = ${sum}`);
+console.log(`${num1} - ${num2} = ${sub}`);
+console.log(`${num1} x ${num2} = ${mult}`);
+console.log(`${num1} / ${num2} = ${div}`);
+
+let aVariable; // creates the variables
+aVariable = 50 // assign a value
+aVariable = 100;
+console.log(aVariable);
+
+let num3 = 5*5
+let num4 = 3.14
+
+let area = num3 * num4
+
+console.log(`${num3} * ${num4} = ${area}`);
+
+
